21b8e2a253fa67e6b048299b4ee98e9cc1919fd5
Isabelle Nascimento <noisabelle06@gmail>
2023-06-27 07:31:55 -0300

    Construção da página

---
+++ controller.js
@@ -0,0 +1,17 @@
+/*
+Cóodigo de validação de formulário de login
+*/
+
+function Validar(){
+    let email = document.getElementById ("email").value;
+    let senha = document.getElementById ("senha").value;
+    /*
+    esse ! significa vasio= email vazio ou senha vazio fazer{ */
+   
+   if(!email || !senha){
+       alert ("Campo de preenchimento obrigatório. Favor preencher");
+   }else{
+       alert("Campos preenchidos com sucesso!");
+   }
+   }
+   
